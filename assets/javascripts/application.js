e44df3f69f07cd6aa4a718523f9112f21538e445
goddyzhao <goddy128@gmail.com>
2012-02-23 16:17:59 +0800

    switch to templates

--- assets/javascripts/application.js
+++ assets/javascripts/application.js
@@ -1,20 +1,64 @@
 ;(function(win, undefined){
 	$(function(){
-		//initialize data-original-title and data-content
-		$(".milestones ul li .content").each(function(){
-			$(this).parent().attr("data-original-title", $(this).html());
+		var templateSource = $("#page-template").html()
+		  , template = Handlebars.compile(templateSource)
+		  , templateUrl = "locales/#{language}.json"
+		  , languageMap = {
+			  	  zh: $("#hd .language li[data-language='zh']")
+			  	, en: $("#hd .language li[data-language='en']")
+		    }
+		  , currentLanguage = "zh"
+		  , $body = $("body");
+		  
+		
+		function switchLanguage(language){
+			$.getJSON(templateUrl.replace("#{language}", language), function(data){
+				if(!data){ return; }
+				render(data);
+			});
+		};
+		
+		function render(data){
+			$body.fadeOut(function(){
+				$("#bd").html(template(data));
+				$("#bd .avatar").css("z-index","100");
+				initPopover();
+				$body.fadeIn();
+			});
+		};
+		
+		function initPopover(){
+			//initialize data-original-title and data-content
+			$(".milestones ul li .content").each(function(){
+				var $this  = $(this)
+				  , $li = $this.parent()
+				  , details = $this.next(".details").html();
+				$li.attr("data-original-title", $this.html());
+				if(!!details){
+					$li.attr("data-content", details)
+				}
+			});
+			
+			$(".milestones ul li").popover({
+				    placement: "bottom"
+				});
+		}
+		
+		//bind events on the language triggers
+		$("#hd .language").click(function(e){
+			var $languageDom = $(e.target)
+			  , language = $languageDom.attr("data-language");
+			if(!language || currentLanguage === language){ return; }
+			languageMap[currentLanguage].removeClass("active");
+			languageMap[language].addClass("active");
+			switchLanguage(language);
+			currentLanguage = language;
 		});
-		$(".milestones ul li").popover({
-			    content: "<a href='http://baidu.com/'>goddyzhao</a>"
-			  , placement: "bottom"
-			});
-		return;
-		$(".milestones ul li")
-			.hover(function(e){
-				$(this).parent().parent().parent().addClass("active");
-			}, function(e){
-				$(this).parent().parent().parent().removeClass("active");
-			});
+		
+		(function init(){
+			switchLanguage(currentLanguage);
+		})();
+		
 		 
 	});
 	
